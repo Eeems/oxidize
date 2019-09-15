2144d7abfe0e4a8dfa46ef23cb85b71a9e021efe
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2019-09-14 23:24:35 -0600

    Fix compile errors and handle the wpa_supplicant service

--- src/wifi.rs
+++ src/wifi.rs
@@ -1,14 +1,12 @@
 extern crate wifiscanner;
-use wifiscanner;
-
 extern crate wpactrl;
-use wpactrl::WpaCtrl;
 
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::io::Read;
 use std::io::Seek;
+use std::io::SeekFrom;
 use std::io::Write;
 use std::process;
 use std::path::Path;
@@ -27,48 +25,70 @@ pub fn init() {
     if Path::new("/lib/systemd/system/wpa_supplicant.service").exists() {
         let mut file = File::open("/lib/systemd/system/wpa_supplicant.service").unwrap();
         let reader = BufReader::new(&file);
-        let mut newtext = "";
+        let mut newtext = "".to_string();
         let mut changed = false;
         for (num, line) in reader.lines().enumerate() {
             let text: String = line.unwrap().chars().collect();
             if text == "ExecStart=/usr/sbin/wpa_supplicant -u\n" {
                 changed = true;
-                newtext = "ExecStart=/usr/sbin/wpa_supplicant -u -O /var/run/wpa_supplicant\n";
+                newtext += "ExecStart=/usr/sbin/wpa_supplicant -u -O /var/run/wpa_supplicant\n";
             } else{
-                newtext += text;
+                newtext += &text;
             }
         }
         if changed {
-            file.seek(SeekFrom::Start(0))?;
+            file.seek(SeekFrom::Start(0));
             file.write_all(newtext.as_bytes());
+            let status = process::Command::new("systemctl")
+                .args(&["restart", "wpa_supplicant"])
+                .status()
+                .expect("Failed to restart wpa_supplicant");
         }
     }
 }
 pub fn state() -> Result<String, String> {
     Ok(read_attribute("operstate")?)
 }
-pub fn scan() -> Result<Vec<Wifi>, Error> {
+pub fn scan() -> Result<Vec<wifiscanner::Wifi>, wifiscanner::Error> {
     return wifiscanner::scan();
 }
-pub fn disconnect() -> Result<String> {
-    let wpa = WpaCtrl::new().open().unwrap();
-    return wpa.request("disconnect").unwrap();
+pub fn disconnect() {
+    let mut wpa = wpactrl::WpaCtrl::new().open().unwrap();
+    wpa.request("disconnect");
 }
-pub fn reconnect() -> Result<String> {
-    let wpa = WpaCtrl::new().open().unwrap();
-    return wpa.request("reconnect").unwrap();
+pub fn reconnect() {
+    let mut wpa = wpactrl::WpaCtrl::new().open().unwrap();
+    wpa.request("reconnect");
 }
-pub fn disable() -> Result<bool> {
+pub fn restart() {
+    disable();
+    enable();
+}
+pub fn disable() -> Result<bool, String> {
     let status = process::Command::new("ip")
-        .arg(&["link", "set", "dev", "wlan0", "down"])
+        .args(&["link", "set", "dev", "wlan0", "down"])
         .status()
         .expect("Failed to disable wifi");
+    if !status.success() {
+        return Ok(false);
+    }
+    let status = process::Command::new("systemctl")
+        .args(&["stop", "wpa_supplicant"])
+        .status()
+        .expect("Failed to stop wpa_supplicant");
     return Ok(status.success());
 }
-pub fn enable() -> Result<bool> {
+pub fn enable() -> Result<bool, String> {
     let status = process::Command::new("ip")
-        .arg(&["link", "set", "dev", "wlan0", "up"])
+        .args(&["link", "set", "dev", "wlan0", "up"])
         .status()
         .expect("Failed to enable wifi");
+    if !status.success() {
+        return Ok(false);
+    }
+    let status = process::Command::new("systemctl")
+        .args(&["start", "wpa_supplicant"])
+        .status()
+        .expect("Failed to start wpa_supplicant");
     return Ok(status.success());
 }
